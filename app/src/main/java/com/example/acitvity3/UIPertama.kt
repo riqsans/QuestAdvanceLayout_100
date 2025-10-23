5cff3026e6cc6e703a1ef92318cb040ab1a66676
riqsans <thrq88@gmail.com>
2025-10-23 20:40:50 +0700

    menambahkan card ke 3

--- app/src/main/java/com/example/acitvity3/UIPertama.kt
+++ app/src/main/java/com/example/acitvity3/UIPertama.kt
@@ -106,7 +106,44 @@ fun ActivitasPertama(modifier: Modifier){
                 }
             }
         }
-
+        Card (modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Green
+            )){
+            Row(){
+                val gambar = painterResource(R.drawable.logo_liverpool)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(100.dp).padding(5.dp)
+                )
+                Spacer(modifier = Modifier.width(30.dp))
+                Column(){
+                    Text(
+                        stringResource(R.string.nama2),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                    Text(
+                        stringResource(R.string.nomor1),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                    Text(
+                        stringResource(R.string.alamat2),
+                        fontSize = 20.sp,
+                        color = Color.Yellow,
+                        modifier = Modifier .padding(top = 10.dp)
+                    )
+                }
+            }
+        }
         Box(
             modifier = Modifier
                 .fillMaxSize()
