06e197463e7a05d5fa79e8dc209204ea37575ce5
riqsans <thrq88@gmail.com>
2025-10-23 12:34:55 +0700

    Menampilkan teks copyright

--- app/src/main/java/com/example/acitvity3/UIPertama.kt
+++ app/src/main/java/com/example/acitvity3/UIPertama.kt
@@ -71,4 +71,10 @@ fun ActivitasPertama(modifier: Modifier){
         Box(
             modifier = Modifier
                 .fillMaxSize()
-        )
+        ){
+            Text(
+                stringResource(R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
+            )
