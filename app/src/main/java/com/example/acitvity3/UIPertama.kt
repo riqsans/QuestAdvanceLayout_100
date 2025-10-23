362df6952e14edcb9f9686ae8a0b4ba5ecd7c40b
riqsans <thrq88@gmail.com>
2025-10-23 12:30:28 +0700

    menambahkan column untuk mengatur nama dan alamat secara vertikal

--- app/src/main/java/com/example/acitvity3/UIPertama.kt
+++ app/src/main/java/com/example/acitvity3/UIPertama.kt
@@ -51,3 +51,4 @@ fun ActivitasPertama(modifier: Modifier){
                     modifier = Modifier.size(100.dp).padding(5.dp)
                 )
                 Spacer(modifier = Modifier.width(30.dp))
+                Column(){
