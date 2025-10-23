3d90358e1d19af2468468586b6ec29bbd986a952
riqsans <thrq88@gmail.com>
2025-10-23 12:31:59 +0700

    Menampilkan nama dengan font cursive dan warna putih

--- app/src/main/java/com/example/acitvity3/UIPertama.kt
+++ app/src/main/java/com/example/acitvity3/UIPertama.kt
@@ -52,3 +52,10 @@ fun ActivitasPertama(modifier: Modifier){
                 )
                 Spacer(modifier = Modifier.width(30.dp))
                 Column(){
+                    Text(
+                        stringResource(R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
